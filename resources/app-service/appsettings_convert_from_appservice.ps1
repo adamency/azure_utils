c91c0a68fb60e315103362a6433e5f9e03a47ad3
adamency <adamency+github@protonmail.com>
2023-02-28 00:25:53 +0100

    Add App Service json App Settings conversion script

---
+++ resources/app-service/appsettings_convert_from_appservice.ps1
@@ -0,0 +1,36 @@
+param (
+    #[Parameter(Mandatory=$true)]
+    #[string]$TenantId,
+
+    #[Parameter(Mandatory=$true)]
+    #[string]$AppClientId,
+
+    #[Parameter(Mandatory=$true)]
+    #[string]$AppPassword,
+    
+    [Parameter(Mandatory = $true)]
+    [string]$ResourceGroup,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Appservice
+)
+
+$app_service_json = $(az webapp config appsettings list -g $ResourceGroup -n $Appservice -o json)
+
+$num_elements = $(echo $app_service_json | jq '. | length' -)
+
+echo "{"
+for ($i = 0 ; $i -lt $num_elements ; $i ++)
+{
+  $name = $(echo $app_service_json | jq "  .[$i].name" -)
+  $value = $(echo $app_service_json | jq "  .[$i].value" -)
+  if ($i -eq ($num_elements - 1))
+  {
+    echo "${name}: ${value},"
+  }
+  else
+  {
+    echo "${name}: ${value}"
+  }
+}
+echo "}"
